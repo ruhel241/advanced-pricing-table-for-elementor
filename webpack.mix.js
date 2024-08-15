135023ee027a4d7114525a271fea7604620c8b7b
MD.RUHEL KHAN <ruhel241@gmail.com>
2024-08-15 21:57:53 +0600

    fix css file

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ let mix = require('laravel-mix');
 mix.setPublicPath('assets');
 
 // mix.copy('resources/images', 'assets/images');
-mix.sass('./resources/sass/apt-pricing-table.scss', './assets/css/aptfe-pricing-table.css');
+mix.sass('./resources/sass/aptfe-pricing-table.scss', './assets/css/aptfe-pricing-table.css');
